8f22cf09b2a22a4bec5c9190b17f2cdf6af7da9c
Tiange Wang <wtgkevin@gmail.com>
2018-08-18 14:28:54 -0700

    API

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,11 +4,46 @@ const users = require('../controllers/users');
 module.exports = function(app) {
     // index page 
     app.get('/', function(req, res) {
-        contests.index(req, res);
+        res.render('pages/');
     });
-
     // about page 
     app.get('/payment', function(req, res) {
-        contests.about(req, res);
+        res.render('pages/payment');
     });
+
+    //----------API BELOW---------
+    // For contests
+    app.get('/contests', function(req, res) {
+        contests.all(req, res);
+    });
+    app.get('/contests/:id', function(req, res) {
+        contests.one(req, res);
+    });
+    app.post('/contests', function(req, res) {
+        contests.create(req, res);
+    });
+    app.put('/contests/:id', function(req, res) {
+        contests.update(req, res);
+    });
+    app.delete('/contests/:id', function(req, res) {
+        contests.remove(req, res);
+    });
+    
+    // For users
+    app.get('/users', function(req, res) {
+        users.all(req, res);
+    });
+    app.get('/users/:id', function(req, res) {
+        users.one(req, res);
+    });
+    app.post('/users', function(req, res) {
+        users.create(req, res);
+    });
+    app.put('/users/:id', function(req, res) {
+        users.update(req, res);
+    });
+    app.delete('/users/:id', function(req, res) {
+        users.remove(req, res);
+    });
+
 }
--- server/controllers/contests.js
+++ server/controllers/contests.js
@@ -1,10 +1,61 @@
 require('../models/contest.js');
 
 module.exports = {
-    index: function(req, res) {
-        res.render('pages/');
+    all: function(req, res){
+        Contest.find({}).sort('createdAt').exec(function(err, contests){
+            if(err){
+                console.log('Something went wrong when getting all contests');
+                res.json({message: 'Error', error: err});
+            }else{
+                res.json({message: 'Success', data: contests});
+            }
+        });
     },
-    about: function(req, res) {
-        res.render('pages/payment');
-    }
+    one: function(req, res){
+        Contest.findOne({_id: req.params.id}, function(err, contest){
+            if(err){
+                console.log('Something went wrong when getting a single contest');
+                res.json({message: 'Error', error: err});
+            }else{
+                res.json({message: 'Success', data: contest});
+            }
+        });
+    },
+    create: function(req, res){
+        Contest.create(req.body, function(err){
+            if(err){
+                console.log('Something went wrong when creating a contest, detail: ', err);
+                res.json({message: 'Error', error: err});   
+            }else{
+                res.redirect('/contests');
+            }
+        });
+    },
+    update: function(req, res){
+        Contest.findByIdAndUpdate({_id: req.params.id}, {$set: req.body}, { runValidators: true }, function(err){
+            if(err){
+                console.log('Something went wrong when updating a contest, detail: ', err);
+                res.json({message: 'Error', error: err});
+            }else{
+                res.redirect(303, '/contests');
+            }
+        });
+    },
+    remove: function(req, res){
+        Contest.findOneAndRemove({_id: req.params.id}, function(err){
+            if(err){
+                console.log('Something went wrong when removing a contest');
+                res.json({message: 'Error', error: err});
+            }else{
+                Contest.find({}, function(err, contests){
+                    if(err){
+                        console.log('Something went wrong when getting all contests');
+                        res.json({message: 'Error', error: err});
+                    }else{
+                        res.json({message: 'Success', data: contests});
+                    }
+                });
+            }
+        });
+    },
 }
