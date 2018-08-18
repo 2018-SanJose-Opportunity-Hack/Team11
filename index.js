6ac861dab23ccba1bbcf6af9386e34f2dc29cdbe
dingzhengfuaa <dingzhengfuaa@gmail.com>
2018-08-18 16:53:40 -0700

    started paypal payments

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var app = express();
+var paypal = require('paypal-rest-sdk');
+paypal.configure({
+  'mode': 'sandbox', //sandbox or live
+  'client_id': 'EBWKjlELKMYqRNQ6sYvFo64FtaRLRR5BdHEESmha49TM',
+  'client_secret': 'EO422dn3gQLgDbuwqTjzrFgFtaRLRR5BdHEESmha49TM'
+});
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
