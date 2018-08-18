0745829dcef85d9a048f73c422ad66041c980c12
Unknown <rudy.wahjudi@gmail.com>
2018-08-18 16:36:22 -0700

    create contest

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use('/public', express.static('public'));
 
 require('./server/config/mongoose.js');
