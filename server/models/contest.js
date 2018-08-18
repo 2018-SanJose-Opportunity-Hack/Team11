f377a0580b4b6ceec787412fe6538746e5ffc137
Tiange Wang <wtgkevin@gmail.com>
2018-08-18 14:19:10 -0700

    stuff

--- server/models/contest.js
+++ server/models/contest.js
@@ -2,7 +2,9 @@ require('../models/user.js');
 
 const ContestSchema = new mongoose.Schema({
     message: {type: String, required: [true, 'Message cannot be blank!']},
+    max_winners: Number,
     winner_cards: [{ img_url: String, value: Number }],
+    time_period: { start_date: Date, end_date: Date },
     users_won: [UserSchema]
 }, {timestamps: true});
 mongoose.model('Contest', ContestSchema);
