d50222d4a99f36eb1f1d74bc32eb9015c6aab701
Kim Vy Vo <kvvo@ucdavis.edu>
2018-08-18 16:29:49 -0700

    register

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 UserSchema = new mongoose.Schema({
     email: {type: String, required: [true, 'Email cannot be blank!']},
+    password: {type: String, required: [true, 'Password cannot be blank!'], minlength: [8, 'Password must be at least 8 characters.']},
     participated_contests: [{ id: String, won: Boolean }]
 }, {timestamps: true});
 mongoose.model('User', UserSchema);
