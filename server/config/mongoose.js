539e7a4f3a587fcf8a06616e0b3a3b08e7417610
Tiange Wang <wtgkevin@gmail.com>
2018-08-18 21:42:40 -0700

    mLab

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,3 +1,3 @@
 mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/earn');
+mongoose.connect('mongodb://earnhack:Hack4Good@ds225902.mlab.com:25902/scratchexpress');
 mongoose.Promise = global.Promise;
